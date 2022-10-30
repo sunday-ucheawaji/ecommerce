fb31d59a4470a536a08423b8885f04dccb359b23
Ucheawaji Sunday <ucheawaji@ext.kobo360.com>
2022-10-30 04:29:17 +0100

    Backup DB from SQLite to Postgres

--- backend/user_auth/models/blacklist.py
+++ backend/user_auth/models/blacklist.py
@@ -4,7 +4,7 @@ from user_auth.models.custom_user import CustomUser
 
 
 class BlackList(models.Model):
-    token = models.CharField(max_length=50)
+    token = models.CharField(max_length=550)
     custom_user = models.ForeignKey(
         CustomUser, on_delete=models.CASCADE, related_name="tokens")
     date_blacklisted = models.DateTimeField(default=timezone.now)
